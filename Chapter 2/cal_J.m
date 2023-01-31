94f775be55a0d9b1e046e420216f3944c1915acf
chaitanya1chawla <103497541+chaitanya1chawla@users.noreply.github.com>
2023-01-31 21:39:13 +0530

    Update cal_J.m

--- Chapter 2/cal_J.m
+++ Chapter 2/cal_J.m
@@ -1,6 +1,6 @@
 % 1b
 function [J] = cal_J(theta_1, theta_2, theta_3)
-    J = [-4*sin(theta_1)-3*sin(theta_1+theta2)-2*sin(theta_1+theta_2+theta_3), -3*sin(theta_1+theta2)-2*sin(theta_1+theta_2+theta_3), -2*sin(theta_1+theta_2+theta_3);
-        4*cos(theta_1)+3*cos(theta_1+theta2)+2*cos(theta_1+theta_2+theta_3), 3*cos(theta_1+theta2)+2*cos(theta_1+theta_2+theta_3), 2*cos(theta_1+theta_2+theta_3);
+    J = [-4*sin(theta_1)-3*sin(theta_1+theta_2)-2*sin(theta_1+theta_2+theta_3), -3*sin(theta_1+theta_2)-2*sin(theta_1+theta_2+theta_3), -2*sin(theta_1+theta_2+theta_3);
+        4*cos(theta_1)+3*cos(theta_1+theta_2)+2*cos(theta_1+theta_2+theta_3), 3*cos(theta_1+theta_2)+2*cos(theta_1+theta_2+theta_3), 2*cos(theta_1+theta_2+theta_3);
         1, 1, 1];
 end
