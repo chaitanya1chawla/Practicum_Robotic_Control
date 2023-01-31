763760e981f78572906b078aed016b3b4e80940d
chaitanya1chawla <103497541+chaitanya1chawla@users.noreply.github.com>
2023-01-31 21:33:20 +0530

    Delete personal.m

--- Chapter 3/personal.m
+++
@@ -1,7 +0,0 @@
-% personal.m
-% Please fill in your name and matrikelnumber. Do not modify anything else.
-% This should be a script file and not a function which initializes the
-% strings for firstname, lastname and matnumber.
-firstname='Chaitanya'; %name z.B. firstname='Max'
-lastname='Chawla'; %family name z.B. lastname='Mustermann'
-matnumber='03741060'; %matrikelnumber z.B. matnumber='12345678'
